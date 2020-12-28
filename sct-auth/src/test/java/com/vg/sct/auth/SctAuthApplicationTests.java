330b52c1ceec4593459ba836082ad3017bc489a4
xieweij <844065343@qq.com>
2020-12-28 13:20:58 +0800

    新增验证中心

---
+++ sct-auth/src/test/java/com/vg/sct/auth/SctAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vg.sct.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SctAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
